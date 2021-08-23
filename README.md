48e7c2175da3745e0e36119cda1f81c25b6f35eb
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-08-23 18:48:00 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Compass
+
+Implement methods in [Direction](src/main/java/com/epam/rd/autotasks/Direction.java) class:
+
+- `ofDegrees` - Returns a Direction instance by input degrees or empty Optional if there is none.
+- `closestToDegrees` - Returns a closest Direction instance to input degrees.
+- `opposite` - Returns a Direction instance that is opposite to this.
+- `differenceDegreesTo` - Returns the difference in degrees between this and input Direction instance.
---
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -0,0 +1,29 @@
+package com.epam.rd.autotasks;
+
+import java.util.Optional;
+
+public enum Direction {
+    N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
+
+    Direction(final int degrees) {
+        this.degrees = degrees;
+    }
+
+    private int degrees;
+
+    public static Optional<Direction> ofDegrees(int degrees) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Direction closestToDegrees(int degrees) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Direction opposite() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int differenceDegreesTo(Direction direction) {
+        throw new UnsupportedOperationException();
+    }
+}
